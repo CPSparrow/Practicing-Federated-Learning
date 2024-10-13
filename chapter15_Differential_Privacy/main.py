14920a807a7c521a81153869ff1726c839604ecd
handsome-coder <1186977762@qq.com>
2024-10-13 13:01:05 +0800

    格式化以及基础修订

--- chapter15_Differential_Privacy/main.py
+++ chapter15_Differential_Privacy/main.py
@@ -7,19 +7,16 @@ import torch, random
 from server import *
 from client import *
 import models, datasets
-
-	
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-
+	
 	parser = argparse.ArgumentParser(description='Federated Learning')
 	parser.add_argument('-c', '--conf', dest='conf')
 	args = parser.parse_args()
 	
-
 	with open(args.conf, 'r') as f:
-		conf = json.load(f)	
-	
+		conf = json.load(f)
 	
 	train_datasets, eval_datasets = datasets.get_dataset("./data/", conf["type"])
 	
@@ -28,10 +25,11 @@ if __name__ == '__main__':
 	
 	for c in range(conf["no_models"]):
 		clients.append(Client(conf, server.global_model, train_datasets, c))
-		
-	print("\n\n")
-	for e in range(conf["global_epochs"]):
 	
+	print("\n\n")
+	acc_store, loss_store = [], []
+	for e in range(conf["global_epochs"]):
+		
 		candidates = random.sample(clients, conf["k"])
 		
 		weight_accumulator = {}
@@ -39,23 +37,22 @@ if __name__ == '__main__':
 		for name, params in server.global_model.state_dict().items():
 			weight_accumulator[name] = torch.zeros_like(params)
 		
-		for c in candidates:
+		for c, idx in enumerate(candidates):
 			diff = c.local_train(server.global_model)
+			print("#{} client done".format(idx))
 			
 			for name, params in server.global_model.state_dict().items():
 				weight_accumulator[name].add_(diff[name])
-				
 		
 		server.model_aggregate(weight_accumulator)
 		
 		acc, loss = server.model_eval()
-		
+		acc_store.append(acc)
+		loss_store.append(loss)
 		print("Epoch %d, acc: %f, loss: %f\n" % (e, acc, loss))
-				
-			
-		
-		
-	
-		
-		
-	
+	plt.figure(dpi=160)
+	plt.figure(1)
+	plt.plot([i + 1 for i in range(len(acc_store))], acc_store)
+	plt.figure(2)
+	plt.plot([i + 1 for i in range(len(loss_store))], loss_store)
+	plt.show()
--- chapter15_Differential_Privacy/models.py
+++ chapter15_Differential_Privacy/models.py
@@ -1,15 +1,15 @@
-
-import torch 
+import torch
 from torchvision import models
 import math
 
+
 def get_model(name="vgg16", pretrained=True):
 	if name == "resnet18":
-		model = models.resnet18(pretrained=pretrained)
+		model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
 	elif name == "resnet50":
-		model = models.resnet50(pretrained=pretrained)	
+		model = models.resnet50(pretrained=pretrained)
 	elif name == "densenet121":
-		model = models.densenet121(pretrained=pretrained)		
+		model = models.densenet121(pretrained=pretrained)
 	elif name == "alexnet":
 		model = models.alexnet(pretrained=pretrained)
 	elif name == "vgg16":
@@ -18,17 +18,18 @@ def get_model(name="vgg16", pretrained=True):
 		model = models.vgg19(pretrained=pretrained)
 	elif name == "inception_v3":
 		model = models.inception_v3(pretrained=pretrained)
-	elif name == "googlenet":		
+	elif name == "googlenet":
 		model = models.googlenet(pretrained=pretrained)
-		
+	
 	if torch.cuda.is_available():
 		return model.cuda()
 	else:
-		return model 
-		
+		return model
+
+
 def model_norm(model_1, model_2):
 	squared_sum = 0
 	for name, layer in model_1.named_parameters():
-	#	print(torch.mean(layer.data), torch.mean(model_2.state_dict()[name].data))
+		#	print(torch.mean(layer.data), torch.mean(model_2.state_dict()[name].data))
 		squared_sum += torch.sum(torch.pow(layer.data - model_2.state_dict()[name].data, 2))
 	return math.sqrt(squared_sum)
